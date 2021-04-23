4db85bfdb1159c3c03dc9e7e13f78ecd05ff7af4
cyberpau <johnpaulomataac@gmail.com>
2021-04-23 23:01:58 +0800

    creating test node

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,18 +1,25 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node {'web.puppet.vm':}
-  dockeragent::node {'db.puppet.vm':}
-  dockeragent::node {'minetest.puppet.vm':}
-  host { 'web.puppet.vm':
-    ensure => present,
-    ip => '172.18.0.2',
-  }
-  host { 'db.puppet.vm':
-    ensure => present,
-    ip => '172.18.0.3',
-  }
-  host { 'minetest.puppet.vm':
-    ensure => present,
-    ip => '172.18.0.4',
-  }
+  dockeragent::node {'test.puppet.vm':}
+
+  # Sample nodes for LinkedIn Learning Puppet Course:
+  #
+  # dockeragent::node {'web.puppet.vm':}
+  # dockeragent::node {'db.puppet.vm':}
+  # dockeragent::node {'minetest.puppet.vm':}
+  #
+
+  # Adding to host
+  # host { 'web.puppet.vm':
+  #   ensure => present,
+  #   ip => '172.18.0.2',
+  # }
+  # host { 'db.puppet.vm':
+  #   ensure => present,
+  #   ip => '172.18.0.3',
+  # }
+  # host { 'minetest.puppet.vm':
+  #   ensure => present,
+  #   ip => '172.18.0.4',
+  # }
 }
