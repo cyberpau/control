7c21803d64a52a7733f0b33b0c6d79a47c34a429
cyberpau <johnpaulomataac@gmail.com>
2021-04-23 23:18:39 +0800

    remove other nodes

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,18 +10,18 @@ node 'master.puppet.vm' {
   }
 }
 
-node 'minetest.puppet.vm' {
-  include role::minecraft_server
-  file {'/root/README':
-    ensure => file,
-    content => "Welcome to ${fqdn} \n",
-  }
-}
+# node 'minetest.puppet.vm' {
+#   include role::minecraft_server
+#   file {'/root/README':
+#     ensure => file,
+#     content => "Welcome to ${fqdn} \n",
+#   }
+# }
 
-node /^web/ {
-  include role::app_server
-}
+# node /^web/ {
+#   include role::app_server
+# }
 
-node /^db/ {
-  include role::db_server
-}
+# node /^db/ {
+#   include role::db_server
+# }
