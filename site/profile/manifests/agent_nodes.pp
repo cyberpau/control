c6367d7f0c40c753773ebc5a727c3f373598de37
cyberpau <johnpaulomataac@gmail.com>
2021-04-23 23:29:30 +0800

    revert comments

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -3,23 +3,23 @@ class profile::agent_nodes {
   dockeragent::node {'test.puppet.vm':}
 
   # Sample nodes for LinkedIn Learning Puppet Course:
-  #
-  # dockeragent::node {'web.puppet.vm':}
-  # dockeragent::node {'db.puppet.vm':}
-  # dockeragent::node {'minetest.puppet.vm':}
-  #
+  
+  dockeragent::node {'web.puppet.vm':}
+  dockeragent::node {'db.puppet.vm':}
+  dockeragent::node {'minetest.puppet.vm':}
+  
 
-  # Adding to host
-  # host { 'web.puppet.vm':
-  #   ensure => present,
-  #   ip => '172.18.0.2',
-  # }
-  # host { 'db.puppet.vm':
-  #   ensure => present,
-  #   ip => '172.18.0.3',
-  # }
-  # host { 'minetest.puppet.vm':
-  #   ensure => present,
-  #   ip => '172.18.0.4',
-  # }
+  # Manually Adding to fully qualified domain names (fqdn) to host
+  host { 'web.puppet.vm':
+    ensure => present,
+    ip => '172.18.0.2',
+  }
+  host { 'db.puppet.vm':
+    ensure => present,
+    ip => '172.18.0.3',
+  }
+  host { 'minetest.puppet.vm':
+    ensure => present,
+    ip => '172.18.0.4',
+  }
 }
